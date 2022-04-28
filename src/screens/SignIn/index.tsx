4f22a4c314082787c4fbf9d08363e8f7e4206777
Mateus Lammel <lammel.mateus@gmail.com>
2022-04-28 20:42:51 -0300

    Splash pronto

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { styles } from "./styles";
+
+const SignIn = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Ola</Text>
+      <TextInput/>
+    </View>
+  );
+};
+
+export default SignIn;
